d99250cb03ce58cbba3e0d74420920080a346b72
a1986775 <a1986775@rle-desktop>
2018-04-16 15:09:55 -0300

    Exercício Feito 3.1

---
+++ src/main/java/Pratica31.java
@@ -0,0 +1,41 @@
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Gustavo
+ */
+public class Pratica31 {
+
+    private static Date inicio, fim;
+    private static String meuNome;
+    private static GregorianCalendar dataNascimento, dataAtual;    
+
+    public static void main(String[] args) {
+
+        inicio = new Date();
+
+        meuNome = "Gustavo Regis Amorim";
+
+        System.out.println(meuNome.toUpperCase());
+
+        System.out.println(meuNome.substring(14) + "," + meuNome.substring(0, 1).toUpperCase() + "." + meuNome.substring(8, 9).toUpperCase() + ".");
+
+        dataNascimento = new GregorianCalendar(1999, Calendar.JUNE, 27);    
+        
+        dataAtual = new GregorianCalendar();
+
+        System.out.println((dataAtual.getTimeInMillis()-dataNascimento.getTimeInMillis())/86400000);
+
+        fim = new Date();
+
+        System.out.println(fim.getTime() - inicio.getTime());
+    }
+}
