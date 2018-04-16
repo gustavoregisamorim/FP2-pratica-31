567954e5462240718e02a2faeb45abf24bb6111b
a1986775 <a1986775@rle-desktop>
2018-04-16 15:19:23 -0300

    Exercício Feito 3.1

--- src/main/java/Pratica31.java
+++ src/main/java/Pratica31.java
@@ -16,7 +16,9 @@ public class Pratica31 {
 
     private static Date inicio, fim;
     private static String meuNome;
-    private static GregorianCalendar dataNascimento, dataAtual;    
+    private static GregorianCalendar dataNascimento = new GregorianCalendar(1999, Calendar.JUNE, 27);
+    private static GregorianCalendar dataAtual = new GregorianCalendar(); 
+    
 
     public static void main(String[] args) {
 
@@ -26,11 +28,7 @@ public class Pratica31 {
 
         System.out.println(meuNome.toUpperCase());
 
-        System.out.println(meuNome.substring(14) + "," + meuNome.substring(0, 1).toUpperCase() + "." + meuNome.substring(8, 9).toUpperCase() + ".");
-
-        dataNascimento = new GregorianCalendar(1999, Calendar.JUNE, 27);    
-        
-        dataAtual = new GregorianCalendar();
+        System.out.println(meuNome.substring(14) + "," + meuNome.substring(0, 1).toUpperCase() + "." + meuNome.substring(8, 9).toUpperCase() + ".");    
 
         System.out.println((dataAtual.getTimeInMillis()-dataNascimento.getTimeInMillis())/86400000);
 
